70f57ea50755e6e54ef32cb477e844053f3971bb
karlkarimoisa <karl.karimoisa@tmd.ee>
2025-05-08 18:46:24 +0300

    paranudsed mis töötavad

--- api/regions.js
+++ api/regions.js
@@ -1,15 +1,5 @@
 // /api/regions.js
 import { Pool } from 'pg';
-import geoRegions from "../src/data/geoRegions"; // otse samast kaustast
-
-export default function handler(req, res) {
-  try {
-    res.status(200).json(geoRegions);
-  } catch (error) {
-    console.error("GeoRegions laadimine ebaõnnestus", error);
-    res.status(500).json({ error: "Server error" });
-  }
-}
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -18,25 +8,20 @@ const pool = new Pool({
 
 export default async function handler(req, res) {
   try {
-   
     const result = await pool.query(`
-      SELECT DISTINCT region, 
-             AVG(lat) AS lat, 
-             AVG(lng) AS lng
+      SELECT DISTINCT region
       FROM price_stats
       WHERE region IS NOT NULL
-      GROUP BY region
+      ORDER BY region
     `);
-    
+
     res.status(200).json(
       result.rows.map(r => ({
         name: r.region,
-        center: [parseFloat(r.lat), parseFloat(r.lng)]
       }))
     );
-    
   } catch (error) {
-    console.error('Andmete laadimine ebaõnnestus', error);
-    res.status(500).json({ error: 'Server error' });
+    console.error('❌ Piirkondade laadimine ebaõnnestus:', error);
+    res.status(500).json({ error: 'Serveri viga' });
   }
 }
